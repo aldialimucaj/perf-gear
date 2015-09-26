714aecce446f395bc1b90ed4feb5ede0bf96aad8
Aldi Alimucaj <aldi.alimucaj@gmail.com>
2015-09-26 23:40:29 +0200

    [TEST] expanded c-example with fibonacci

--- api-tests/c-example2.c
+++ api-tests/c-example2.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "perf-gear.h"
 
+
 int add(int a, int b) {
     pg_m_item_t* m1 = PG_NEW_MEASUREMENT("examples/c/example2/add", PG_MEASUREMENT_TYPE_HIT);
     PG_INCREASE_HIT(m1);
@@ -15,6 +16,12 @@ int add2(int a, int b) {
     return a + b;
 }
 
+int fibonacci_at(int i) {
+    PG_AUTO_HIT("examples/c/example2/fibonacci_at");
+    if(i <= 2) return 1;
+    return fibonacci_at(i - 1) + fibonacci_at(i - 2);
+}
+
 int main() {
     printf("Starting c-example2.c\n");
     
@@ -26,9 +33,14 @@ int main() {
     PG_START(&config);
     
     printf("1 + 2 = %i\n", add(1, 2));
+   
     printf("1 + 2 = %i\n", add2(1, 2));
     printf("3 + 4 = %i\n", add2(3, 4));
     printf("5 + 6 = %i\n", add2(5, 6));
+    
+    printf("fibonacci @ 5 = %i\n", fibonacci_at(5));
+    printf("fibonacci @ 9 = %i\n", fibonacci_at(9));
+    printf("fibonacci @ 27 = %i\n", fibonacci_at(27));
 
     PG_STOP();
 }
